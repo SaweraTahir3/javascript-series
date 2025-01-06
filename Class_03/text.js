30384c950aba4d628db733b89f7c3999df80de74
Sawera Tahir <141661220+SaweraTahir3@users.noreply.github.com>
2025-01-06 10:20:32 +0000

    Operations done

---
+++ Class_03/text.js
@@ -0,0 +1,95 @@
+// Arithmetic Operations
+
+console.log(2 + 5);
+console.log(2 - 5);
+console.log(2 * 5);
+console.log(2 / 5);
+console.log(2 ** 5);
+console.log(2 % 5);
+
+// Assignment Operations
+
+let x = 10 
+x += 5
+console.log(x);
+x *= 2;
+console.log(x);
+x -= 5 ;
+console.log(x);
+x /= 5 ;
+console.log(x);
+x %= 5 ;
+console.log(x);
+
+// Comparison Operations
+ 
+let a = 2 ;
+let b = 2 ;
+let equal = (a == b);
+console.log(equal); // equal to ==
+
+let c = 5 ;
+let d = 7 ;
+let notequal = (c != d);
+console.log(notequal); // not equal !
+
+let e = 10 ;
+let f = 8 ;
+let greater = (e > f);
+console.log(greater); //greater than >
+
+let g = 4 ;
+let h = 15 ;
+let less = (g < h);
+console.log(less); // less than <
+
+let i = 10 ;
+let j = 20 ;
+let k = (i >= j);
+console.log(k); // Greater than or equal >=
+
+let l = 10 ;
+let m = 20 ;
+let n = (l <= m );
+console.log(n); // Less than or equal <=
+
+
+// Logical Operations
+
+let o = 15 ;
+let p = 10 ;
+let q = (o && p);
+console.log(q); //And &&
+
+let  r =  false;
+let s = true ;
+let t = (r || s);
+console.log(t); //or ||
+
+let u = 16;
+let v = 12 ;
+let w =(u != v);
+console.log(w); // notequal !
+
+
+// increment 
+
+let gamecount = 50 ;
+gamecount++ ;   // (Note : increments after this statement) // Post-increment
+console.log(gamecount);
+
+let appcount = 50 ;
+++appcount ;    // (Note: increments before this statement) // Pre-increment
+console.log(appcount);
+
+// Decrement
+
+let counter = 25;
+counter-- ;    // (Note : decrements after this statement) // Post-decrement
+console.log(counter);
+
+let count = 25 ;
+--count ;      // (Note: decrements before this statement) // Pre-decrement
+console.log(count);
+
+
